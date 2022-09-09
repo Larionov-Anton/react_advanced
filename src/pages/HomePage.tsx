fa06a087e1be63e3d0064345cd6955a5fdadaeb2
Larionov-Anton <larionovanton05@gmail.com>
2022-09-09 16:34:22 +0300

    Add search input, dropdown for results live search
    Create custom hooks 'useDebounse' - saves the input value with a delay
    in 'debounced'
    When the value changes, a request is sent to the server and the data
    from the response is displayed in dropdown
    Repeated request when returning focus

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDebounse } from "../hooks/debounce";
 import { useSearchUsersQuery } from "../store/github/github.api";
 
 export function HomePage() {
+	const [search, setSearch] = useState('')
+	const [dropdown, setDropdown] = useState(false)
+	const debounced = useDebounse(search, 500)
+	const {isError, isLoading, data} = useSearchUsersQuery(debounced, {
+		skip: debounced.length < 3,
+		refetchOnFocus: true
+	})
+	
 
-	const {isError, isLoading, data} = useSearchUsersQuery('vladilen')
-	console.log(data)
+	useEffect(() => {
+		setDropdown(debounced.length > 3 && data?.length! > 0)
+	}, [debounced, data])
 
 	return (
-		<div>Home Page</div>
+		<div className="flex justify-center pt-10 mx-auto h-screen w-screen" >
+			{ isError && <p className="text-center text-red-600" >Something went wrong...</p>}
+
+			<div className="relative w-[560px]" >
+				<input 
+					type="text"
+					className="border py-2 px-4 w-full h-[42px] mb-2" 
+					placeholder="Search for Github username..."
+					value={search}
+					onChange={e => setSearch(e.target.value)}
+				/>
+
+				{dropdown && <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white overflow-y-scroll">
+					{isLoading && <p className="text-center" >Loading...</p>}
+					{data?.map(user => (
+						<li
+							key={user.id}
+							className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+						>{user.login}</li>
+					))}
+				</ul>}
+			</div>
+
+		</div>
 	)
 }
